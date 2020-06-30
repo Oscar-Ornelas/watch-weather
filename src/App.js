e0f7bd8870986fee0c36f3832c16c58a0f1a64e1
Oscar Ornelas <oscarornelas98123@gmail.com>
2020-06-29 19:41:57 -0500

    Stylize weather card

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 import WeatherCard from './components/WeatherCard';
 import rainy_cloudy from './imgs/rainy_cloudy.png';
+import partly_cloudy from './imgs/partly_cloudy.png';
+import sunny from './imgs/sunny.png';
 
 function App() {
   return (
-    <div className="App">
-      <WeatherCard day="Mon" highTemperature="87" lowTemperature="63" imageUrl={rainy_cloudy}/>
+    <div className="app">
+      <WeatherCard day="Mon" maxTemperature="87" minTemperature="63" imageUrl={rainy_cloudy}/>
+      <WeatherCard day="Tue" maxTemperature="87" minTemperature="63" imageUrl={partly_cloudy}/>
+      <WeatherCard day="Wed" maxTemperature="87" minTemperature="63" imageUrl={rainy_cloudy}/>
+      <WeatherCard day="Thu" maxTemperature="87" minTemperature="63" imageUrl={partly_cloudy}/>
+      <WeatherCard day="Fri" maxTemperature="87" minTemperature="63" imageUrl={sunny}/>
+      <WeatherCard day="Sat" maxTemperature="87" minTemperature="63" imageUrl={partly_cloudy}/>
+      <WeatherCard day="Sun" maxTemperature="87" minTemperature="63" imageUrl={sunny}/>
     </div>
   );
 }
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -5,8 +5,8 @@ function WeatherCard(props) {
     <div className="weather-card">
       <p className="weather-card-day">{props.day}</p>
       <div className="weather-card-temperatures">
-        <p className="weather-card-high-temperature">{props.highTemperature}</p>
-        <p className="weather-card-low-temperature">{props.lowTemperature}</p>
+        <p className="weather-card-max-temperature">{props.maxTemperature}&#176;</p>
+        <p className="weather-card-min-temperature">{props.minTemperature}&#176;</p>
       </div>
       <img src={props.imageUrl} className="weather-card-image"/>
     </div>
