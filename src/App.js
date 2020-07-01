73ec43257e84509c9f7db3a16804dd6f27639ef9
Oscar Ornelas <oscarornelas98123@gmail.com>
2020-06-30 20:14:00 -0500

    Pull weather info from weathermap api and opencage api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import WeatherCard from './components/WeatherCard';
 import rainy_cloudy from './imgs/rainy_cloudy.png';
@@ -6,15 +6,59 @@ import partly_cloudy from './imgs/partly_cloudy.png';
 import sunny from './imgs/sunny.png';
 
 function App() {
+  const WEATHER_MAP_API_KEY = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
+  const OPENCAGE_API_KEY = process.env.REACT_APP_OPENCAGE_API_KEY;
+  const [location, setLocation] = useState({});
+  const [weatherInfo, setWeatherInfo]  = useState([]);
+
+  useEffect(() => {
+    if(window.navigator.geolocation) {
+      window.navigator.geolocation
+      .getCurrentPosition(position => {
+        getLocation(position.coords.latitude, position.coords.longitude);
+        getWeatherInfo(position.coords.latitude, position.coords.longitude);
+      }, console.log);
+    }
+  }, []);
+
+  function getWeatherInfo(latitude, longitude) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={current,minutely,hourly}&units=imperial&appid=${WEATHER_MAP_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.daily);
+      setWeatherInfo(data.daily);
+    })
+  }
+
+  function getLocation(latitude, longitude) {
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${OPENCAGE_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      const location = data.results[0].components;
+      setLocation({city: location.city, stateCode: location.state_code, countryCode: location.country_code})
+    })
+  }
+
+  const weatherCards = weatherInfo && (
+    weatherInfo.map(day => {
+      const date = new Date(day.dt * 1000).toGMTString();
+      const dayOfWeek = date.substring(0, 3);
+
+      return (
+        <WeatherCard
+          key={day.dt}
+          day={dayOfWeek}
+          maxTemperature={day.temp.max}
+          minTemperature={day.temp.min}
+          imageUrl={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+        />
+      )
+    })
+  )
+
   return (
     <div className="app">
-      <WeatherCard day="Mon" maxTemperature="87" minTemperature="63" imageUrl={rainy_cloudy}/>
-      <WeatherCard day="Tue" maxTemperature="87" minTemperature="63" imageUrl={partly_cloudy}/>
-      <WeatherCard day="Wed" maxTemperature="87" minTemperature="63" imageUrl={rainy_cloudy}/>
-      <WeatherCard day="Thu" maxTemperature="87" minTemperature="63" imageUrl={partly_cloudy}/>
-      <WeatherCard day="Fri" maxTemperature="87" minTemperature="63" imageUrl={sunny}/>
-      <WeatherCard day="Sat" maxTemperature="87" minTemperature="63" imageUrl={partly_cloudy}/>
-      <WeatherCard day="Sun" maxTemperature="87" minTemperature="63" imageUrl={sunny}/>
+      {weatherCards}
     </div>
   );
 }
