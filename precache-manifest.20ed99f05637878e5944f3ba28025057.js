6bdf151c0945fed8e38e08e507f043bc080ed63f
Oscar Ornelas <oscarornelas98123@gmail.com>
2020-07-01 19:49:16 -0500

    Updates

---
+++ precache-manifest.20ed99f05637878e5944f3ba28025057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c5fbe4a70f6ccd82107cd85d51dbde",
+    "url": "/watch-weather/index.html"
+  },
+  {
+    "revision": "cab91fbd3265427e4c7e",
+    "url": "/watch-weather/static/css/main.8443d5ea.chunk.css"
+  },
+  {
+    "revision": "f35ccf565f40d9f831bc",
+    "url": "/watch-weather/static/js/2.f8fa4510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watch-weather/static/js/2.f8fa4510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab91fbd3265427e4c7e",
+    "url": "/watch-weather/static/js/main.9e3daac0.chunk.js"
+  },
+  {
+    "revision": "e92aaff06c395a76b8bb",
+    "url": "/watch-weather/static/js/runtime-main.0e0864bc.js"
+  }
+]);
